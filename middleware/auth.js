cc0c7932d926507eebd16e9628d5919252d2ff52
hamid eslami <heslami32@gmail.com>
2020-07-13 12:43:32 +0430

    normalize path in auth middleware with regex

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,11 +1,23 @@
-const unprotectedUrls = ['/login/','/login', '/register/', '/register']
+const unprotectedUrls = ['/login', '/register']
 const COOKIE_TOKEN = 'token'
 
 export default function(ctx) {
-    const path = ctx.route.path
+    let path = ctx.route.path
     const token = ctx.$storage.getCookie(COOKIE_TOKEN)
 
+    function normalize (p) {
+        const regex = /\/$/gm
+        const subst = ''
+        
+        const result = p.replace(regex, subst)
+        
+        return result
+    }
+    
+    path = normalize(path)
+
     const isProtected = !unprotectedUrls.includes(path)
+
     if (!token && isProtected) {
         ctx.redirect(302, '/login')
     } else if (token && !isProtected) {

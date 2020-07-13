bfbee79cf000b165589d1feb6bffcf192b8ce643
hamid eslami <heslami32@gmail.com>
2020-07-13 12:16:36 +0430

    fix auth middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,4 +1,4 @@
-const unprotectedUrls = ['/login', '/register']
+const unprotectedUrls = ['/login/','/login', '/register/', '/register']
 const COOKIE_TOKEN = 'token'
 
 export default function(ctx) {
@@ -6,10 +6,13 @@ export default function(ctx) {
     const token = ctx.$storage.getCookie(COOKIE_TOKEN)
 
     const isProtected = !unprotectedUrls.includes(path)
+    console.log({path})
+    console.log({isProtected})
     if (!token && isProtected) {
+        console.log('redirect to login')
         ctx.redirect(302, '/login')
-    }
-    if (token && !isProtected) {
+    } else if (token && !isProtected) {
+        console.log('redirect to home')
         ctx.redirect(302, '/')
     }
 }

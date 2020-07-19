4acaf2f24d8582ecaa974e6dc7dc36bbf99edb2b
hamid eslami <heslami32@gmail.com>
2020-07-19 13:27:12 +0430

    refactor - remove unused code in helper

--- utils/helpers.js
+++ utils/helpers.js
@@ -67,20 +67,4 @@ export const normalizeUrl= (urlObj) => {
     })
 
     return result
-    // var urlParams;
-    // let normalizedString = () => {
-    //     var match,
-    //     pl     = /\+/g,  // Regex for replacing addition symbol with a space
-    //     search = /([^&=]+)=?([^&]*)/g,
-    //     decode = function (s) { return decodeURIComponent(s.replace(pl, " ")); },
-    //     query  = window.location.search.substring(1);
-
-    //     urlParams = {};
-
-    //     while (match = search.exec(query))
-    //     urlParams[decode(match[1])] = decode(match[2]);
-
-    //     return urlParams
-    // }
-    // return normalizedString
 }

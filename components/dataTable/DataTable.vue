8820bb6baea5a4ab03602cd2b441a4ffb3426da9
hamid eslami <heslami32@gmail.com>
2020-07-20 16:36:29 +0430

    wip - orders data table

--- components/dataTable/DataTable.vue
+++ components/dataTable/DataTable.vue
@@ -8,45 +8,18 @@
         :headers="headers"
         :items="data"
         item-key="id"
-        class="elevation-1"
         :search="search"
+        class="cell-height"
       >
         <template v-slot:top>
           <v-text-field v-model="search" label="Search" class="mx-4" />
         </template>
         <template v-slot:body="{ items }">
           <tbody>
-            <tr v-for="(item, i) in items" :key="i">
-              <td>{{ item.id }}</td>
-              <td>{{ item.route }}</td>
-              <td>{{ item.departingDate }}</td>
-              <td>{{ item.createdDate }}</td>
-              <td>{{ item.provider + ' - ' + item.airline }}</td>
-              <td>{{ item.pax+' P' }}</td>
-              <td>{{ item.netPrice }}</td>
-              <td>{{ item.departingConfCode }}</td>
-              <td>{{ item.departingOrderStatus }}</td>
-              <td>
-                <v-chips class="success white--text rounded pa-2">{{ item.departingOrderStatus }}</v-chips>
-              </td>
-              <td>{{ item.paymentStatus }}</td>
-              <td></td>
-              <span class="order-icon rounded white elevation-1">
-                <v-icon>
-                  mdi-airplane
-                </v-icon>
-              </span>
+            <tr v-for="(item, i) in items" :key="i" v-html="orderFileds(item)">
             </tr>
           </tbody>
         </template>
-        <!-- <template v-slot:item.actions="{ item }">
-          <v-icon small class="mr-2" @click="editItem(item)">
-            mdi-pencil
-          </v-icon>
-          <v-icon small @click="deleteItem(item)">
-            mdi-delete
-          </v-icon>
-        </template> -->
       </v-data-table>
     </v-expansion-panel-content>
   </v-expansion-panel>
@@ -90,12 +63,82 @@ export default {
             value.toString().toLocaleUpperCase().indexOf(search) !== -1
           )
         },
+        orderFileds(item) {
+          if (item.returning) {
+            return `
+              <td>
+                ${item.id}
+                <br/>
+                ${item.phone}
+              </td>
+              <td>
+                ${item.route}
+                <br/>
+                ${item.route}
+              </td>
+              <td>${item.departingDate} <br/> ${item.returningDate}</td>
+              <td>${item.createdDate}</td>
+              <td>${item.provider} - ${item.airline}</td>
+              <td>${item.pax} P</td>
+              <td>
+                ${item.netPrice}
+                <br/>
+                ${item.salesPrice}
+              </td>
+              <td>${item.departingConfCode} <br/> ${item.returningConfCode}</td>
+              <td>
+                <v-chips class="success white--text rounded py-1 px-2">${item.departingOrderStatus}</v-chips>
+                <br/>
+                <v-chips class="success white--text rounded py-1 px-2">${item.returningOrderStatus}</v-chips>
+              </td>
+              <td>${item.paymentStatus}</td>
+              <td></td>
+              <span class="order-icon rounded white elevation-1">
+                <v-icon>
+                  mdi-airplane
+                </v-icon>
+              </span>`
+          } else {
+            return `
+              <td>
+                ${item.id}
+                <br/>
+                ${item.phone}
+              </td>
+              <td>
+                ${item.route}
+              </td>
+              <td>${item.departingDate}</td>
+              <td>${item.createdDate}</td>
+              <td>${item.provider} - ${item.airline}</td>
+              <td>${item.pax} P</td>
+              <td>
+                ${item.netPrice}
+                <br/>
+                ${item.salesPrice}
+              </td>
+              <td>${item.departingConfCode}</td>
+              <td>
+                <v-chips class="success white--text rounded py-1 px-2">${item.departingOrderStatus}</v-chips>
+              </td>
+              <td>${item.paymentStatus}</td>
+              <td></td>
+              <span class="order-icon rounded white elevation-1">
+                <v-icon>
+                  mdi-airplane
+                </v-icon>
+              </span>`
+          }
+        }
     }
 }
 </script>
 
 <style lang="scss" scoped>
 .data-table {
+  .v-data-table {
+    line-height: 2 !important;
+  }
   .order-icon {
     background-color: #fff;
     border: 1px solid grey;

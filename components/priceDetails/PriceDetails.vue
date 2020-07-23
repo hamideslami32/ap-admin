112f0ebba38b8a3987b2e9f48bdaae7a7491438e
Hamid Eslami <heslami32@gmail.com>
2020-07-23 05:00:19 +0430

    wip - order detail page

---
+++ components/priceDetails/PriceDetails.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="price-details rounded">
+    <v-card height="100%" class="d-flex">
+      <div class="d-flex">
+        <div v-for="(item, i) in data" :key="i" class="d-flex pa-2">
+          <div>{{ item.title }}</div>
+          <div>{{ item.value }}</div>
+          <v-divider vertical class="px-2" />
+        </div>
+      </div>
+      <v-spacer />
+      <div>
+        <span>Benefit:</span>
+        <span>20000 T</span>
+      </div>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    // data: {
+    //   type: Object,
+    //   require : true
+    // }
+  },
+  data() {
+    return {
+      data: [
+        {
+          title: 'Total Price',
+          value: '12,000,000 T'
+        },
+        {
+          title: 'Discount',
+          value: '12,000 T'
+        },
+        {
+          title: 'Purchased Price',
+          value: '12,000,000 T'
+        },
+        {
+          title: 'Mark Up/Down',
+          value: '3%'
+        },
+        {
+          title: 'Sales Price',
+          value: '12,000,000 T'
+        },
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.price-details {
+  width: 100%;
+  height: 100px;
+}
+</style>
---
+++ pages/orders/detail/index.vue
@@ -0,0 +1,88 @@
+<template>
+  <v-layout>
+    <v-flex>
+      <div class="d-flex justify-space-between px-4 align-center user-info">
+        <span v-for="(item, i) in data.userData" :key="i">{{ item.title }}: {{ item.value }}</span>
+      </div>
+      <div>
+        <v-tabs
+          v-model="tab"
+          height="60"
+          background-color="white"
+          centered
+        >
+          <v-tabs-slider />
+          <v-tab href="#tab-1">
+            Ticket Detail
+          </v-tab>
+          <v-tab href="#tab-2">
+            Refund Details
+          </v-tab>
+          <v-tab href="#tab-3">
+            Customer Support
+          </v-tab>
+          <v-tab href="#tab-4">
+            Customer Support
+          </v-tab>
+        </v-tabs>
+      </div>
+      <div class="main pa-4">
+        <OrderWrapper />
+        <v-expansion-panels class="my-4" :value="openExpansionPanel" :multiple="true">
+          <CanceledOrderWrapper />
+        </v-expansion-panels>
+        <OrderWrapper />
+        <PriceDetails class="mt-4" />
+      </div>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+import OrderWrapper from '~/components/orderWrapper/OrderWrapper'
+import CanceledOrderWrapper from '~/components/canceledOrderWrapper/CanceledOrderWrapper'
+import PriceDetails from '~/components/priceDetails/PriceDetails'
+
+export default {
+  components: {
+    OrderWrapper,
+    CanceledOrderWrapper,
+    PriceDetails
+  },
+  data() {
+    return {
+      data: {
+        userData: [
+          {
+            title: 'User',
+            value: 'Maedeh Masoumi'
+          },
+          {
+            title: 'Phone',
+            value: '09125277216'
+          },
+          {
+            title: 'Orders',
+            value: '3 - 9'
+          },
+          {
+            title: 'Order Number',
+            value: 'IF34988'
+          },
+          {
+            title: 'Date of Issue',
+            value: '1399-03-12 - 11:45'
+          },
+        ]
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .user-info {
+    height: 60px;
+    background-color: $lightGray;
+  }
+</style>

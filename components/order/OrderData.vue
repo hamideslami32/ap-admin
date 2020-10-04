ea3fe2739e7e976be4935ece0f1adc8a36b59b1e
Hamid Eslami <heslami32@gmail.com>
2020-10-04 17:43:46 +0330

    wip: orders table

--- components/order/OrderData.vue
+++ components/order/OrderData.vue
@@ -1,47 +1,47 @@
 <template>
   <fragment>
-    <template v-if="data.returning">
+    <template v-if="data.firstOrder.flights.length > 1">
       <td>
-        {{ data.id }}
+        {{ data.order._id }}
         <br>
         <nuxt-link :to="{ path: '/orders', query: { phone: data.phone } }">
-          {{ data.phone }}
+          {{ data.phone || '09366182745' }}
         </nuxt-link>
       </td>
       <td>
-        <nuxt-link :to="{ path: '/orders', query: { 'route-origin': data.origin, 'route-dest': data.dest } }">
-          {{ data.origin.toUpperCase() }}-{{ data.dest.toUpperCase() }}
+        <nuxt-link :to="{ path: '/orders', query: { 'route-origin': data.firstOrder.flights[0].departureAirport, 'route-dest': data.firstOrder.flights[0].arrivalAirport } }">
+          {{ data.firstOrder.flights[0].departureAirport.toUpperCase() }}-{{ data.firstOrder.flights[0].arrivalAirport.toUpperCase() }}
         </nuxt-link>
         <br>
-        <nuxt-link :to="{ path: '/orders', query: { 'route-origin': data.dest, 'route-dest': data.origin } }">
-          {{ data.dest.toUpperCase() }}-{{ data.origin.toUpperCase() }}
+        <nuxt-link :to="{ path: '/orders', query: { 'route-origin': data.firstOrder.flights[1].departureAirport, 'route-dest': data.firstOrder.flights[1].arrivalAirport } }">
+          {{ data.firstOrder.flights[1].departureAirport.toUpperCase() }}-{{ data.firstOrder.flights[1].arrivalAirport.toUpperCase() }}
         </nuxt-link>
       </td>
-      <td>{{ data.departingDate }} <br> {{ data.returningDate }}</td>
-      <td>{{ data.createdDate }}</td>
+      <td>{{ data.firstOrder.flights[0].departureDate }} <br> {{ data.firstOrder.flights[1].departureDate }}</td>
+      <td>{{ data.issueDate }}</td>
       <td>
-        {{ data.provider }} - {{ data.airline }}
+        {{ data.firstOrder.flights[0].providerKey }} - {{ data.firstOrder.flights[0].airline }}
         <br>
-        {{ data.provider }} - {{ data.airline }}
+        {{ data.firstOrder.flights[1].providerKey }} - {{ data.firstOrder.flights[1].airline }}
       </td>
       <td>{{ data.pax }} P</td>
       <td>
-        {{ data.netPrice }}
-        <br>
-        {{ data.salesPrice }}
+        {{ data.price }}
+        <!-- <br>
+        {{ data.salesPrice }} -->
       </td>
       <td>{{ data.departingConfCode }} <br> {{ data.returningConfCode }}</td>
       <td>
         <v-chip class="success white--text rounded py-1 mb-1 px-2">
-          {{ data.departingOrderStatus }}
+          {{ data.order.status }}
         </v-chip>
         <br>
         <v-chip class="success white--text rounded py-1 px-2">
-          {{ data.returningOrderStatus }}
+          {{ data.order.status }}
         </v-chip>
       </td>
-      <td :class="getColor(data.paymentStatus)">
-        {{ data.paymentStatus }}
+      <td :class="getColor(data.paymentStatus || 'Success')">
+        {{ data.paymentStatus || 'Success'}}
       </td>
       <td class="d-flex flex-column justify-space-around">
         <v-btn class="secondary white--text" small @click="$router.push('/orders/detail')">
@@ -54,36 +54,36 @@
     </template>
     <template v-else>
       <td>
-        {{ data.id }}
+        {{ data.order._id }}
         <br>
         <nuxt-link :to="{ path: '/orders', query: { phone: data.phone } }">
           {{ data.phone }}
         </nuxt-link>
       </td>
       <td>
-        <nuxt-link :to="{ path: '/orders', query: { 'route-origin': data.origin, 'route-dest': data.dest } }">
-          {{ data.origin.toUpperCase() }}-{{ data.dest.toUpperCase() }}
+        <nuxt-link :to="{ path: '/orders', query: { 'route-origin': data.firstOrder.flights[0].departureAirport, 'route-dest': data.firstOrder.flights[0].arrivalAirport } }">
+          {{ data.firstOrder.flights[0].departureAirport.toUpperCase() }}-{{ data.firstOrder.flights[0].arrivalAirport.toUpperCase() }}
         </nuxt-link>
       </td>
-      <td>{{ data.departingDate }}</td>
-      <td>{{ data.createdDate }}</td>
+      <td>{{ data.firstOrder.flights[0].departureDate }}</td>
+      <td>{{ data.issueDate }}</td>
       <td>
-        {{ data.provider }} - {{ data.airline }}
+        {{ data.firstOrder.flights[0].providerKey }} - {{ data.firstOrder.flights[0].airline }}
       </td>
       <td>{{ data.pax }} P</td>
       <td>
-        {{ data.netPrice }}
-        <br>
-        {{ data.salesPrice }}
+        {{ data.price }}
+        <!-- <br>
+        {{ data.salesPrice }} -->
       </td>
       <td>{{ data.departingConfCode }}</td>
       <td>
         <v-chip class="success white--text rounded py-1 px-2">
-          {{ data.departingOrderStatus }}
+          {{ data.order.status }}
         </v-chip>
       </td>
-      <td :class="getColor(data.paymentStatus)">
-        {{ data.paymentStatus }}
+      <td :class="getColor(data.paymentStatus || 'Success')">
+        {{ data.paymentStatus || 'Success'}}
       </td>
       <td class="d-flex flex-column justify-space-around">
         <v-btn class="secondary white--text" small @click="$router.push('/orders/detail')">
@@ -104,6 +104,9 @@ export default {
         type: Object,
         required: true
       }
+    },
+    mounted() {
+      console.log(this.data.firstOrder)
     },
     methods: {
       getColor(string) {
